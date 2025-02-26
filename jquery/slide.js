b1974b062b30f5765c85c29ca845e0a66b0d230c
jong_min <whdals0525@gmail.com>
2025-02-26 10:30:32 +0900

    add

---
+++ jquery/slide.js
@@ -0,0 +1,49 @@
+$(document).ready(() => {
+  //왼쪽방향 화살표 클릭
+  const no = $("#slide").find(".active").index();
+  let active = true;
+  $("#btn button")
+    .eq(0)
+    .click(() => {
+      if (active) {
+        active = false;
+        $("#slide").css("transition", "left 1s");
+        $("#slide").css("left", 0);
+        $("#slide section")
+          .eq(no - 1)
+          .addClass("active")
+          .siblings()
+          .removeClass("active");
+        setTimeout(callback_1, 1000);
+      }
+    });
+  const callback_1 = () => {
+    $("#slide").css("transition", "none");
+    $("#slide").css("left", "-1300px");
+    $("#slide").prepend($("#slide").children().last());
+    active = true;
+  };
+
+  //오른쪽방향 화살표 클릭
+  $("#btn button")
+    .eq(1)
+    .click(() => {
+      if (active) {
+        active = false;
+        $("#slide").css("transition", "left 1s");
+        $("#slide").css("left", "-2600px");
+        $("#slide section")
+          .eq(no + 1)
+          .addClass("active")
+          .siblings()
+          .removeClass("active");
+        setTimeout(callback_2, 1000);
+      }
+    });
+  const callback_2 = () => {
+    $("#slide").css("transition", "none");
+    $("#slide").css("left", "-1300px");
+    $("#slide").append($("#slide").children().first());
+    active = true;
+  };
+}); //............js end
--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,8 @@
 let index = 0;
 const menuItems = document.querySelectorAll(".menu > li");
 const totalItems = menuItems.length;
+const hireBtn = document.querySelector("#hire-btn");
+const hrModal = document.querySelector(".hr-modal");
 
 const intervalId = setInterval(function () {
   if (index < totalItems) {
@@ -10,3 +12,7 @@ const intervalId = setInterval(function () {
     clearInterval(intervalId);
   }
 }, 300);
+
+hireBtn.addEventListener("click", function () {
+  hrModal.classList.toggle("on");
+});
