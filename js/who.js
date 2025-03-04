568abd397a81420be4a695bc53767e4b03c839a4
jong_min <whdals0525@gmail.com>
2025-03-04 22:56:48 +0900

    add

--- js/who.js
+++ js/who.js
@@ -1,10 +1,10 @@
 const images = [
-  "../img/who/who-img1.jpeg",
-  "../img/who/who-img2.jpg",
-  "../img/who/who-img3.jpeg",
-  "../img/who/who-img4.jpeg",
-  "../img/who/who-img5.jpeg",
-  "../img/who/who-img6.jpeg",
+  "/img/who/who-img1.jpeg",
+  "/img/who/who-img2.jpg",
+  "/img/who/who-img3.jpeg",
+  "/img/who/who-img4.jpeg",
+  "/img/who/who-img5.jpeg",
+  "/img/who/who-img6.jpeg",
 ];
 
 const pfElement = document.querySelector(".who-pf");
