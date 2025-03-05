93f4e5e75193617620883da51624f65ea73e1bae
jong_min <whdals0525@gmail.com>
2025-03-05 17:44:49 +0900

    add

--- js/canvas.js
+++ js/canvas.js
@@ -1,38 +1,60 @@
 let canvas;
 let ctx;
 let whatSt = document.querySelector(".what-st");
+let gameStartBtn = document.querySelector(".what-game-btn"); // 게임 시작 버튼
+let countdownDiv = document.createElement("div"); // 카운트다운 표시를 위한 div
+countdownDiv.style.position = "absolute";
+countdownDiv.style.top = "50%";
+countdownDiv.style.left = "50%";
+countdownDiv.style.transform = "translate(-50%, -50%)";
+countdownDiv.style.fontSize = "50px";
+countdownDiv.style.color = "white";
+countdownDiv.style.textAlign = "center";
+whatSt.appendChild(countdownDiv);
 
-// 캔버스 생성 및 추가
+let gameReady = true; // 게임 대기 상태
+let gameOver = false;
+let score = 0;
+
 canvas = document.createElement("canvas");
-divTag = document.createElement("div");
 ctx = canvas.getContext("2d");
 canvas.width = 1000;
 canvas.height = 550;
 whatSt.appendChild(canvas);
 
 let hamsterImage, tackImage;
-let gameOver = false; // gameOver값이 true이면 게임종료
-let score = 0;
 
 // 햄스터 좌표
 let hamX = canvas.width / 2 - 50;
 let hamY = canvas.height - 99;
 
-let bulletList = []; // 총알 저장 리스트
+let bulletList = [];
+let enemyList = [];
+let enemyImages = [
+  "./img/lang-ic/html.png",
+  "./img/lang-ic/css.png",
+  "./img/lang-ic/js.png",
+  "./img/lang-ic/jquery.png",
+  "./img/lang-ic/react.png",
+  "./img/lang-ic/mysql.png",
+  "./img/lang-ic/github.png",
+  "./img/lang-ic/word.png",
+  "./img/lang-ic/power.png",
+  "./img/lang-ic/excel.svg",
+];
+
 function Bullet() {
   this.x = 0;
   this.y = 0;
-  this.alive = true; // true면 살아있는 총알
+  this.alive = true;
   this.init = function () {
     this.x = hamX;
     this.y = hamY - 80;
-
     bulletList.push(this);
   };
   this.update = function () {
     this.y -= 7;
   };
-
   this.checkHit = function () {
     for (let i = 0; i < enemyList.length; i++) {
       if (
@@ -48,53 +70,33 @@ function Bullet() {
   };
 }
 
-function generateRandomValue(min, max) {
-  let randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
-  return randomNum;
-}
-
-let enemyList = [];
-let enemyImages = [
-  "./img/lang-ic/html.png",
-  "./img/lang-ic/css.png",
-  "./img/lang-ic/js.png",
-  "./img/lang-ic/jquery.png",
-  "./img/lang-ic/react.png",
-  "./img/lang-ic/mysql.png",
-  "./img/lang-ic/github.png",
-  "./img/lang-ic/word.png",
-  "./img/lang-ic/power.png",
-  "./img/lang-ic/excel.svg",
-];
-
 function Enemy() {
   this.x = 0;
   this.y = 0;
-  this.image = null; // 랜덤 이미지를 저장할 변수
+  this.image = null;
   this.init = function () {
     this.y = 0;
     this.x = generateRandomValue(100, canvas.width - 150);
-
-    // 이미지 리스트에서 랜덤으로 하나를 선택
     this.image = new Image();
     let randomIndex = generateRandomValue(0, enemyImages.length - 1);
     this.image.src = enemyImages[randomIndex];
-
     enemyList.push(this);
   };
   this.update = function () {
     this.y += 1;
-
     if (this.y >= canvas.height - 100) {
       gameOver = true;
     }
   };
 }
 
+function generateRandomValue(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 function loadImage() {
   hamsterImage = new Image();
   hamsterImage.src = "./img/card/Hamster.png";
-
   tackImage = new Image();
   tackImage.src = "./img/icon/rotated-tack.png";
 }
@@ -118,28 +120,23 @@ function createBullet() {
 }
 
 function createEnemy() {
-  const interval = setInterval(function () {
-    let e = new Enemy();
-    e.init();
+  setInterval(function () {
+    if (!gameReady && !gameOver) {
+      let e = new Enemy();
+      e.init();
+    }
   }, 1500);
 }
 
 function update() {
-  if ("ArrowRight" in keysDown) {
-    hamX += 8;
-  }
-  if ("ArrowLeft" in keysDown) {
-    hamX -= 8;
-  }
+  if (gameReady || gameOver) return;
 
-  if (hamX <= 0) {
-    hamX = 0;
-  }
-  if (hamX >= canvas.width - 100) {
-    hamX = canvas.width - 100;
-  }
+  if ("ArrowRight" in keysDown) hamX += 8;
+  if ("ArrowLeft" in keysDown) hamX -= 8;
 
-  // 총알의 y좌표 업데이트 함수
+  if (hamX <= 0) hamX = 0;
+  if (hamX >= canvas.width - 100) hamX = canvas.width - 100;
+
   for (let i = 0; i < bulletList.length; i++) {
     if (bulletList[i].alive) {
       bulletList[i].update();
@@ -153,37 +150,59 @@ function update() {
 }
 
 function render() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height); // 캔버스 초기화
-
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.drawImage(hamsterImage, hamX, hamY, 100, 99);
   ctx.fillText(`Score: ${score}`, canvas.width - 150, 50);
   ctx.font = "30px Arial";
   ctx.fillStyle = "white";
 
-  for (let i = 0; i < bulletList.length; i++) {
-    if (bulletList[i].alive) {
-      ctx.drawImage(tackImage, bulletList[i].x, bulletList[i].y, 100, 100);
+  if (!gameReady) {
+    for (let i = 0; i < bulletList.length; i++) {
+      if (bulletList[i].alive) {
+        ctx.drawImage(tackImage, bulletList[i].x, bulletList[i].y, 100, 100);
+      }
     }
-  }
 
-  for (let i = 0; i < enemyList.length; i++) {
-    const enemyWidth = 100;
-    const enemyHeight = 100;
-    ctx.drawImage(
-      enemyList[i].image, // 랜덤 이미지를 사용
-      enemyList[i].x,
-      enemyList[i].y,
-      enemyWidth,
-      enemyHeight
-    );
+    for (let i = 0; i < enemyList.length; i++) {
+      ctx.drawImage(
+        enemyList[i].image,
+        enemyList[i].x,
+        enemyList[i].y,
+        100,
+        100
+      );
+    }
   }
 }
 
 function main() {
   update();
   render();
-  requestAnimationFrame(main);
+  if (!gameOver) requestAnimationFrame(main);
 }
+
+function startCountdown() {
+  let countdown = 3;
+  countdownDiv.innerHTML = countdown;
+  countdownDiv.style.fontSize = "150px";
+  const interval = setInterval(() => {
+    countdown--;
+    countdownDiv.innerHTML = countdown;
+    if (countdown === 0) {
+      clearInterval(interval);
+      countdownDiv.style.display = "none";
+      gameReady = false;
+      main();
+    }
+  }, 1000);
+}
+
+gameStartBtn.addEventListener("click", function () {
+  if (gameReady) {
+    startCountdown();
+    gameStartBtn.style.display = "none"; // 시작 버튼 숨기기
+  }
+});
 
 loadImage();
 setupKeyboardListener();
