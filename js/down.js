754caa163041436067f708301afa86bfa7333501
jong_min <whdals0525@gmail.com>
2025-03-13 19:18:37 +0900

    add

--- js/down.js
+++
@@ -1,6 +0,0 @@
-// document.getElementById("download").addEventListener("click", function () {
-//   window.open(
-//     "https://github.com/sonjongm1/portfolio/raw/main/pdf/resume.pdf",
-//     "_blank"
-//   ); // 새 탭에서 열기
-// });
