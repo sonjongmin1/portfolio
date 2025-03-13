4a6d1093dda017b963b66fa772a9626256468d2b
jong_min <whdals0525@gmail.com>
2025-03-13 17:50:09 +0900

    add

--- js/down.js
+++ js/down.js
@@ -1,6 +1,6 @@
-document.getElementById("download").addEventListener("click", function () {
-  window.open(
-    "https://github.com/sonjongm1/portfolio/raw/main/pdf/resume.pdf",
-    "_blank"
-  ); // 새 탭에서 열기
-});
+// document.getElementById("download").addEventListener("click", function () {
+//   window.open(
+//     "https://github.com/sonjongm1/portfolio/raw/main/pdf/resume.pdf",
+//     "_blank"
+//   ); // 새 탭에서 열기
+// });
