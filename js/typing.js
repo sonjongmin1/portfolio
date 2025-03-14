d1da76a2efbe6f1b827b0eab6d95d5c80b07de3b
jong_min <whdals0525@gmail.com>
2025-03-14 13:23:54 +0900

    add

--- js/typing.js
+++ js/typing.js
@@ -9,14 +9,18 @@ function typeText() {
     a += c[i]; // 한 글자씩 추가
     typing.textContent = a; // 요소의 텍스트 업데이트
     i++;
-    setTimeout(typeText, 150); // 0.3초 간격으로 실행
+    setTimeout(typeText, 200); // 0.3초 간격으로 실행
   } else {
     // 문자열 끝에 도달하면 초기화
     setTimeout(() => {
       a = "";
       i = 0;
-      typeText(); // 다시 타이핑 시작
-    }, 150); // 타이핑 종료 후 1초 기다림
+      typing.classList.add("blink"); // Add blink class for the last "|" character
+      setTimeout(() => {
+        typing.classList.remove("blink"); // Remove blink class after the reset
+        setTimeout(typeText, 2500); // 3초 후 다시 타이핑 시작
+      }, 1000); // 타이핑 종료 후 1초 기다림
+    }, 1000); // 타이핑 종료 후 1초 기다림
   }
 }
 
